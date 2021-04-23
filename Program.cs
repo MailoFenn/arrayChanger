3987f1f16a1febdf1a8c3a10693b7984cdfb1758
Mailo_Fenn <nikitakulkov7@gmail.com>
2021-04-23 22:36:00 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+            int[] sub_arr_up = new int[arr.GetLength(1)];
+            int[] sub_arr_down = new int[arr.GetLength(1)];
+            for(int i = 0; i < arr.GetLength(0); i++)
+            {
+                for(int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if(i == 0)
+                    {
+                        sub_arr_up[j] = arr[i, j];
+                    }
+                    if(i == arr.GetLength(0) - 1)
+                    {
+                        sub_arr_down[j] = arr[i, j];
+                    }
+                }
+            }
+
+            for(int i = 0; i < arr.GetLength(0); i++)
+            {
+                for(int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if(i == 0)
+                    {
+                        arr[i, j] = sub_arr_down[j];
+                    }
+                    if(i == arr.GetLength(0) - 1)
+                    {
+                        arr[i, j] = sub_arr_up[j] ;
+                    }
+                }
+            }
+        }
+    }
+}
